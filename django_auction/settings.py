17e9ff96aed5afae36950b0cf602e1da74e6544b
Martin Paju <martinpaju@hotmail.com>
2020-10-20 14:00:24 +0300

    Add registration form

--- django_auction/settings.py
+++ django_auction/settings.py
@@ -147,7 +147,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_URL = 'login' 
+# LOGIN_URL = 'login' 
 LOGIN_REDIRECT_URL = '/' 
-LOGOUT_URL = 'logout'
+# LOGOUT_URL = 'logout'
 # LOGOUT_REDIRECT_URL = '/'
