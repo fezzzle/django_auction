af3cd749e7851d8dadc1ff36d38eb6d7cca070cc
Martin Paju <martinpaju@hotmail.com>
2020-10-20 10:31:41 +0300

    Init

---
+++ auction/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+import logging
+
+logger = logging.getLogger("mylogger")
+
+
+class Auction(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    min_value = models.IntegerField()
+    date_added = models.DateTimeField(datetime.now, blank=True)
+    is_active = models.BooleanField(default=True)
+    total_auction_duration = models.IntegerField()
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, 
+                              related_name="auction_winner",
+                              related_query_name="auction_winner")
+    final_value = models.IntegerField(blank=True, null=True)
+
+    def resolve(self):
+        if self.is_active:
+            if self.has_expired():
+                highest_bid = Bid.objects.filter(auction=self).order_by('-amount').first()
+                logger.info(f"HIGHTEST BID: {highest_bid}")
+                if highest_bid:
+                    self.winner = highest_bid.bidder
+                    self.final_value = highest_bid.amount
+                self.is_active = False
+                self.save()
+
+    def has_expired(self):
+        now = timezone.now()
+        # logger.info(f"NOW IS: {now}")
+        auction_end = self.date_added + timedelta(minutes=self.total_auction_duration)
+        # logger.info(f"AUCTION END IS: {auction_end}")
+        # logger.info(f"TIME NOW IS BIGGER THAN AUCTION END: {now > auction_end}")
+        if now > auction_end:
+            return True
+        else:
+            return False
+    
+    @property
+    def seconds_remaining(self):
+        if self.is_active:
+            now = datetime.now(timezone.utc)
+            expire = self.date_added + timedelta(minutes=self.total_auction_duration)
+            time_remaining = (expire - now).total_seconds()
+            return time_remaining
+        else:
+            return 0
+
+    def __str__(self):
+        return self.title
+
+
+
+class Bid(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_added = models.DateTimeField()
+    amount = models.IntegerField(default=1)
+
+    @property
+    def highest_bid(self):
+        if self.amount:
+            return self.amount
+
---
+++ auction/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render
+from .models import Auction, Bid
+from datetime import datetime, timezone
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+
+from django.contrib.auth.decorators import login_required
+
+
+import logging
+
+logger = logging.getLogger("mylogger")
+
+def index(request):
+    auctions = Auction.objects.all()
+    current_user = request.user
+    return render(request, 'auction/index.html', {'auctions': auctions, 'user': current_user})
+
+@login_required
+def detail(request, auction_id):
+    auction = get_object_or_404(Auction, pk=auction_id)
+    bid = Bid.objects.filter(auction=auction)
+    auction.resolve()
+    if bid:
+        bid = bid.first().amount
+    if request.user == auction.author:
+        own_auction = True
+        return render(request, "auction/detail.html", {"auction": auction, "own_auction": own_auction, 'bid': bid})
+    return render(request, "auction/detail.html", {"auction": auction, 'bid': bid})
+
+
+@login_required
+def create(request):
+    # submit_button = request.POST.get('submit_button')
+    submit_button = request.POST.get('submit_button')
+    # logger.info(f"Submit buttons type: {type(submit_button)}")
+    # logger.info(f"User sending the request: {request.user}")
+    if submit_button:
+        try:
+            title = request.POST['title']
+            description = request.POST['description']
+            min_value = request.POST['min_value']
+            duration = request.POST['duration']
+            logger.info(f"values: {title, description, min_value}")
+            
+            if not title or not description or not min_value:
+                raise KeyError
+        except KeyError:
+            return render(request, "auction/create.html", {"error_message": "Please fill all the fields!"})
+        else:
+            auction = Auction()
+            auction.author = request.user
+            auction.title = title
+            auction.description = description
+            auction.min_value = min_value
+            auction.date_added = timezone.now()
+            auction.total_auction_duration = duration
+            auction.save()
+
+            return HttpResponseRedirect(reverse('auction:detail', args=(auction.id,)))
+
+    else:
+        return render(request, "auction/create.html")
+@login_required
+def auctions(request):
+    auctions_list = Auction.objects.all().order_by('-date_added')
+    # logger.info(f"AUCTIONS LIST: {auctions_list}")
+    for a in auctions_list:
+        a.resolve()
+    return render(request, "auction/auctions.html", {"auctions_list": auctions_list})
+
+@login_required
+def delete_auctions(request):
+    if request.user.is_superuser:
+        auctions = Auction.objects.all().delete()
+        logger.info(f"All auctions deleted")
+    else:
+        logger.info("You need to be a superuser")
+
+    return render(request, "auction/deleted.html")
+    # return HttpResponseRedirect(reverse('auction:detail', args=(auction.id,)))
+
+@login_required
+def my_auctions(request):
+    my_auctions = Auction.objects.filter(author=request.user)
+    for a in my_auctions:
+        a.resolve()
+    return render(request, "auction/my_auctions.html", {'my_auctions': my_auctions})
+
+@login_required
+def bid(request, auction_id):
+    auction = get_object_or_404(Auction, pk=auction_id)
+    auction.resolve()
+
+    bid = Bid.objects.filter(bidder=request.user).filter(auction=auction).first()
+
+    if not auction.is_active:
+        return render(request, "auction/detail.html", {'auction': auction, 'error_message': 'Auction has been expired'})
+    
+    try:
+        bid_amount = int(request.POST['amount'])
+
+        if not bid_amount or int(bid_amount) < auction.min_value:
+            raise ValueError
+
+        if not bid:
+            bid = Bid()
+            bid.auction = auction
+            bid.bidder = request.user
+
+        if bid:
+            if bid_amount <= bid.amount:
+                return render(request, "auction/detail.html", {'auction': auction, 'bid': bid.highest_bid, 'error_message': 'You need to enter a bigger bid than the previous amount!'})
+
+        bid.amount = int(bid_amount)
+        bid.time_added = datetime.now(timezone.utc)
+    except ValueError:
+        logger.info(f"VALUE ERROR AT ROW 123: {ValueError}")
+        return render(request, "auction/detail.html", {'auction': auction, 'error_message': 'You have entered invalid input or less than min value'})
+
+    else:
+        bid.save()
+        return HttpResponseRedirect(reverse('auction:auctions', args=()))
+        
+
+
+    # return render(request, "auction/index.html")
+    # return HttpResponseRedirect(reverse('auction:index'))
+
