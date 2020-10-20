34fee83d772c78a92ea414224f69632d48b29b34
Krauno <rauno.kark@gmail.com>
2020-10-20 13:57:57 +0300

    a

--- django_auction/urls.py
+++ django_auction/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auction.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
 
     # path('login', include('django.contrib.auth.urls'))
 ]
