61c89e9b27c8a0d187ca3972e28e1be0aabbd6aa
Martin Paju <martinpaju@hotmail.com>
2020-11-03 14:55:45 +0200

    Feat
    See Users on admin page

--- auction/admin.py
+++ auction/admin.py
@@ -1,5 +1,21 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
-from auction.models import Auction, Bid
+from auction.models import Auction, Bid, CustomUser
 
+
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = (
+        *BaseUserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Custom Field Heading',  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'is_bot_flag',
+                ),
+            },
+        ),
+    )
+
+admin.site.register(CustomUser, BaseUserAdmin)
 admin.site.register(Auction)
 admin.site.register(Bid)
