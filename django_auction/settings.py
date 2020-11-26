98f6ad9c4227f3154fd63b433350987ddd10c1f4
Martin Paju <martinpaju@hotmail.com>
2020-11-26 18:52:39 +0200

    Refactor
    Add Makefile && start using pipenv

--- django_auction/settings.py
+++ django_auction/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret! 50 Chars long
-SECRET_KEY = '00000000000000000000000000000000000000000000000001'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
